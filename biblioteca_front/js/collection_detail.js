7a81d278bbec9397a12c0efb67018cadad47dc6e
İrem Türen <72929400+iremturen@users.noreply.github.com>
2024-09-29 23:14:43 +0300

    biblioteca

---
+++ biblioteca_front/js/collection_detail.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const back_logo = document.getElementById('back_logo');
+    const back_text = document.getElementById('back_text');
+    
+    back_logo.addEventListener('click', () => {
+        window.location.href = 'collections.html';
+    });
+
+    back_text.addEventListener('click', () => {
+        window.location.href = 'collections.html';
+    });
+    
+});
+
