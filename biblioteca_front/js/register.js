ff3252dcc7bfb318cac5d2fd8711865c2a6ac927
İrem Türen <72929400+iremturen@users.noreply.github.com>
2025-01-05 15:52:00 +0300

    feat: country code in register input

--- biblioteca_front/js/register.js
+++ biblioteca_front/js/register.js
@@ -12,6 +12,11 @@ const register_button = document.getElementById('register_button');
 const sign_in = document.getElementById('sign_in');
 const error_msg = document.getElementById('error_msg');
 
+const iti = window.intlTelInput(tel_input, {
+    separateDialCode: true, 
+    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.15/js/utils.js"
+});
+
 sign_in.addEventListener('click', () => {
     window.location.href = 'login.html';
 });
@@ -28,9 +33,37 @@ register_button.addEventListener('click',  async () => {
     const birthDate = bday_input.value.trim();
     const password = password_input.value.trim();
 
+    const currentDate = new Date().toISOString().split('T')[0];
+    bday_input.setAttribute('max', currentDate);
+    bday_input.addEventListener('change', () => {
+        const birthDate = bday_input.value;
+        if (birthDate > currentDate) {
+            error_msg.style.marginLeft = "-10px";
+            error_msg.style.display = "flex";
+            error_msg.innerHTML = "Enter a valid date.";
+        } else {
+            error_msg.style.display = "none";
+        }
+    });
+
+    if (password.length < 8 || password.length > 12) {
+        error_msg.style.marginLeft = "25px";
+        error_msg.style.display = "flex";
+        error_msg.innerHTML = "Password must be between 8 and 12 characters.";
+        return;
+    }
+
     if (!username || !name || !surname || !email || !telNo || !country || !city || !birthDate || !password) {
-        error_msg.style.marginLeft = "-15px";
+        error_msg.style.marginLeft = "100px";
+        error_msg.style.display="flex";
         error_msg.innerHTML = "Please fill in all fields!";
+        return;
+    }
+
+    if (!iti.isValidNumber()) {
+        error_msg.style.marginLeft = "-10px";
+        error_msg.style.display = "flex";
+        error_msg.innerHTML = "Please enter a valid phone number with country code.";
         return;
     }
 
@@ -56,7 +89,8 @@ register_button.addEventListener('click',  async () => {
         });
 
         if (response.ok) {
-            error_msg.style.marginLeft = "-10px";
+            error_msg.style.marginLeft = "100px";
+            error_msg.style.display="flex";
             error_msg.innerHTML = "Registration is successful!";
             setTimeout(() => {
                 window.location.href = 'login.html'; 
@@ -64,9 +98,20 @@ register_button.addEventListener('click',  async () => {
         } else {
             const errorText = await response.text();
             console.error("Registration failed: ", errorText);
+
+            if (errorText.includes('Duplicate entry')) {
+                error_msg.style.marginLeft = "-10px";
+                error_msg.style.display = "flex";
+                error_msg.innerHTML = "This email, username or phone number are already registered.";
+            } else {
+                error_msg.style.marginLeft = "80px";
+                error_msg.style.display = "flex";
+                error_msg.innerHTML = "Registration failed. Please try again.";
+            }
         }
     } catch (error) {
         console.error("Error:", error);
+        error_msg.style.display="flex";
         error_msg.innerHTML = "An error has occurred. Please try again later.";
     }
 });
